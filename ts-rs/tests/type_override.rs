773695b5a866c549eba9b76440bd2d3a34d06d74
Moritz Bischof <moritz.bischof1@gmail.com>
2021-11-08 21:34:22 +0100

    improve tests for type overrides

--- ts-rs/tests/type_override.rs
+++ ts-rs/tests/type_override.rs
@@ -4,6 +4,10 @@ use std::time::Instant;
 
 use ts_rs::TS;
 
+
+struct Unsupported<T>(T);
+struct Unsupported2;
+
 #[derive(TS)]
 struct Override {
     a: i32,
@@ -11,12 +15,16 @@ struct Override {
     b: i32,
     #[ts(type = "string")]
     x: Instant,
+    #[ts(type = "string")]
+    y: Unsupported<Unsupported<Unsupported2>>,
+    #[ts(type = "string | null")]
+    z: Option<Unsupported2>
 }
 
 #[test]
 fn test() {
     assert_eq!(
         Override::inline(),
-        "{ a: number, b: 0 | 1 | 2, x: string, }"
+        "{ a: number, b: 0 | 1 | 2, x: string, y: string, z: string | null, }"
     )
 }
