df9ac26c7f88ebee95272d5e300e568c91f0f52d
LightQuantum <self@lightquantum.me>
2022-09-13 21:18:35 +0800

    Support kebab-case rename

--- macros/src/attr/mod.rs
+++ macros/src/attr/mod.rs
@@ -20,6 +20,7 @@ pub enum Inflection {
     Snake,
     Pascal,
     ScreamingSnake,
+    Kebab,
 }
 
 impl Inflection {
@@ -33,6 +34,7 @@ impl Inflection {
             Inflection::Snake => string.to_snake_case(),
             Inflection::Pascal => string.to_pascal_case(),
             Inflection::ScreamingSnake => string.to_screaming_snake_case(),
+            Inflection::Kebab => string.to_kebab_case(),
         }
     }
 }
@@ -41,15 +43,18 @@ impl TryFrom<String> for Inflection {
     type Error = Error;
 
     fn try_from(value: String) -> Result<Self> {
-        Ok(match &*value.to_lowercase().replace("_", "") {
-            "lowercase" => Self::Lower,
-            "uppercase" => Self::Upper,
-            "camelcase" => Self::Camel,
-            "snakecase" => Self::Snake,
-            "pascalcase" => Self::Pascal,
-            "screamingsnakecase" => Self::ScreamingSnake,
-            _ => syn_err!("invalid inflection: '{}'", value),
-        })
+        Ok(
+            match &*value.to_lowercase().replace("_", "").replace("-", "") {
+                "lowercase" => Self::Lower,
+                "uppercase" => Self::Upper,
+                "camelcase" => Self::Camel,
+                "snakecase" => Self::Snake,
+                "pascalcase" => Self::Pascal,
+                "screamingsnakecase" => Self::ScreamingSnake,
+                "kebabcase" => Self::Kebab,
+                _ => syn_err!("invalid inflection: '{}'", value),
+            },
+        )
     }
 }
 
