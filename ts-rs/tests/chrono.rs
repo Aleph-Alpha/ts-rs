3ba1bbf5daa81cdfcfdaf41451334e22919394e5
varonroy <varonroy@gmail.com>
2023-07-09 12:18:38 +0100

    Added impls for chrono::Month and chrono::Weekday

--- ts-rs/tests/chrono.rs
+++ ts-rs/tests/chrono.rs
@@ -1,7 +1,8 @@
 #![cfg(feature = "chrono-impl")]
 
 use chrono::{
-    Date, DateTime, Duration, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime, Utc,
+    Date, DateTime, Duration, FixedOffset, Local, Month, NaiveDate, NaiveDateTime, NaiveTime, Utc,
+    Weekday,
 };
 use ts_rs::TS;
 
@@ -19,10 +20,12 @@ fn chrono() {
             DateTime<FixedOffset>,
         ),
         duration: Duration,
+        month: Month,
+        weekday: Weekday,
     }
 
     assert_eq!(
         Chrono::decl(),
-        "interface Chrono { date: [string, string, string, string], time: string, date_time: [string, string, string, string], duration: string, }"
+        "interface Chrono { date: [string, string, string, string], time: string, date_time: [string, string, string, string], duration: string, month: string, weekday: string, }"
     )
 }
