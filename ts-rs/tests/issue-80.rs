837bc44df2c696ec1ef535ad3c34c09fd6363588
Moritz <moritz.bischof1@gmail.com>
2023-08-07 15:16:36 +0200

    add test for issue #80

---
+++ ts-rs/tests/issue-80.rs
@@ -0,0 +1,19 @@
+use ts_rs::TS;
+use serde::Serialize;
+
+#[derive(TS, Serialize)]
+#[ts(export)]
+pub enum SomeTypeList {
+    Value1 {
+        #[serde(skip)]
+        #[ts(skip)]
+        skip_this: String,
+    },
+    Value2
+}
+
+#[test]
+fn issue_80() {
+    let ty = SomeTypeList::inline();
+    assert_eq!(ty, r#"{ "Value1": {  } } | "Value2""#);
+}
