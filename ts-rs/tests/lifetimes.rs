9f38240017660509f135f8968b59663c3bf20ff2
darthturska <aatu.kajasto@gmail.com>
2023-12-10 15:00:35 +0200

    fix: Include borrowed type's type arguments in typescript

--- ts-rs/tests/lifetimes.rs
+++ ts-rs/tests/lifetimes.rs
@@ -10,3 +10,25 @@ fn contains_borrow() {
 
     assert_eq!(S::decl(), "interface S { s: string, }")
 }
+
+#[test]
+fn contains_borrow_type_args() {
+    #[derive(TS)]
+    #[allow(dead_code)]
+    struct B<'a, T: 'a> {
+        a: &'a T,
+    }
+
+    #[derive(TS)]
+    #[allow(dead_code)]
+    struct A<'a> {
+        a: &'a &'a &'a Vec<u32>,                        //Multiple References
+        b: &'a Vec<B<'a, u32>>,                         //Nesting
+        c: &'a std::collections::HashMap<String, bool>, //Multiple type args
+    }
+
+    assert_eq!(
+        A::decl(),
+        "interface A { a: Array<number>, b: Array<B<number>>, c: Record<string, boolean>, }"
+    );
+}
