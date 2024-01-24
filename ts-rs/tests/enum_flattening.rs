56bdef6c968eb1c370af67439776e67ce75f4e00
Gustavo <gustavo.shigueo@proton.me>
2024-01-24 14:38:52 -0300

    Make test more robust

--- ts-rs/tests/enum_flattening.rs
+++ ts-rs/tests/enum_flattening.rs
@@ -6,19 +6,24 @@ use ts_rs::TS;
 fn externally_tagged() {
     #[derive(Serialize, TS)]
     struct Foo {
+        qux: i32,
         #[serde(flatten)]
         baz: Bar,
+        biz: Option<String>,
     }
 
     #[derive(Serialize, TS)]
     #[allow(dead_code)]
     enum Bar {
-        Baz { a: i32, a2: String, },
+        Baz { a: i32, a2: String },
         Biz { b: bool },
-        Buz { c: String },
+        Buz { c: String, d: Option<i32> },
     }
 
-    assert_eq!(Foo::inline(), r#"{ "Baz": { a: number, a2: string, }, } | { "Biz": { b: boolean, }, } | { "Buz": { c: string, }, }"#)
+    assert_eq!(
+        Foo::inline(),
+        r#"{ qux: number, biz: string | null, } & ({ "Baz": { a: number, a2: string, } } | { "Biz": { b: boolean, } } | { "Buz": { c: string, d: number | null, } })"#
+    )
 }
 
 #[test]
@@ -26,21 +31,25 @@ fn externally_tagged() {
 fn adjacently_tagged() {
     #[derive(Serialize, TS)]
     struct Foo {
+        one: i32,
         #[serde(flatten)]
         baz: Bar,
+        qux: Option<String>,
     }
 
     #[derive(Serialize, TS)]
     #[allow(dead_code)]
     #[serde(tag = "type", content = "stuff")]
     enum Bar {
-        Baz { a: i32, a2: String, },
+        Baz { a: i32, a2: String },
         Biz { b: bool },
-        Buz { c: String },
+        Buz { c: String, d: Option<i32> },
     }
 
-    assert_eq!(Foo::inline(), r#"{ "type": "Baz", "stuff": { a: number, a2: string, }, } | { "type": "Biz", "stuff": { b: boolean, }, } | { "type": "Buz", "stuff": { c: string, }, }"#)
-
+    assert_eq!(
+        Foo::inline(),
+        r#"{ one: number, qux: string | null, } & ({ "type": "Baz", "stuff": { a: number, a2: string, } } | { "type": "Biz", "stuff": { b: boolean, } } | { "type": "Buz", "stuff": { c: string, d: number | null, } })"#
+    )
 }
 
 #[test]
@@ -48,6 +57,8 @@ fn adjacently_tagged() {
 fn internally_tagged() {
     #[derive(Serialize, TS)]
     struct Foo {
+        qux: Option<String>,
+
         #[serde(flatten)]
         baz: Bar,
     }
@@ -56,12 +67,15 @@ fn internally_tagged() {
     #[allow(dead_code)]
     #[serde(tag = "type")]
     enum Bar {
-        Baz { a: i32, a2: String, },
+        Baz { a: i32, a2: String },
         Biz { b: bool },
-        Buz { c: String },
+        Buz { c: String, d: Option<i32> },
     }
 
-    assert_eq!(Foo::inline(), r#"{ "type": "Baz", a: number, a2: string, } | { "type": "Biz", b: boolean, } | { "type": "Buz", c: string, }"#)
+    assert_eq!(
+        Foo::inline(),
+        r#"{ qux: string | null, } & ({ "type": "Baz", a: number, a2: string, } | { "type": "Biz", b: boolean, } | { "type": "Buz", c: string, d: number | null, })"#
+    )
 }
 
 #[test]
@@ -77,10 +91,14 @@ fn untagged() {
     #[allow(dead_code)]
     #[serde(untagged)]
     enum Bar {
-        Baz { a: i32, a2: String, },
+        Baz { a: i32, a2: String },
         Biz { b: bool },
         Buz { c: String },
     }
 
-    assert_eq!(Foo::inline(), r#"{ a: number, a2: string, } | { b: boolean, } | { c: string, }"#)
+    assert_eq!(
+        Foo::inline(),
+        r#"{ a: number, a2: string, } | { b: boolean, } | { c: string, }"#
+    )
 }
+
