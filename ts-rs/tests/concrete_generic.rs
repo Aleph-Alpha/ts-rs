7c45e3fe63c7c3a0bd3a35df011e05be9fa62458
Moritz Bischof <moritz.bischof1@gmail.com>
2024-03-12 18:57:51 +0100

    organize tests

--- ts-rs/tests/concrete_generic.rs
+++ ts-rs/tests/concrete_generic.rs
@@ -1,51 +1,58 @@
 #![allow(unused)]
 
-use ts_rs::TS;
+mod issue_261 {
+    use ts_rs::TS;
 
-trait Driver {
-    type Info: TS;
-}
+    trait Driver {
+        type Info: TS;
+    }
 
-#[derive(TS)]
-#[ts(export, export_to = "concrete_generic/")]
-struct Foo {
-    x: i32
-}
+    struct TsDriver;
+    impl Driver for TsDriver {
+        type Info = String;
+    }
 
-struct TsDriver;
-impl Driver for TsDriver {
-    type Info = String;
-}
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/")]
+    struct OtherInfo {
+        x: i32,
+    }
 
-#[derive(TS)]
-#[ts(export, export_to = "concrete_generic/")]
-struct OtherDriver;
-impl Driver for OtherDriver {
-    type Info = Foo;
-}
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/")]
+    struct OtherDriver;
+    impl Driver for OtherDriver {
+        type Info = OtherInfo;
+    }
 
-#[derive(TS)]
-#[ts(export, export_to = "concrete_generic/", concrete(T = TsDriver))]
-struct MyStruct<T: Driver> {
-    u: T::Info,
-}
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/", concrete(T = TsDriver))]
+    struct Consumer1<T: Driver> {
+        info: T::Info,
+    }
 
-#[derive(TS)]
-#[ts(export, export_to = "concrete_generic/", concrete(T = OtherDriver))]
-struct OtherStruct<T: Driver> {
-    u: T::Info,
-    x: T,
-}
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/", concrete(T = OtherDriver))]
+    struct Consumer2<T: Driver> {
+        info: T::Info,
+        driver: T,
+    }
 
+    #[test]
+    fn concrete_generic_param() {
+        assert_eq!(
+            Consumer1::<TsDriver>::decl(),
+            "type Consumer1 = { info: string, };"
+        );
+        // `decl` must use the concrete generic, no matter what we pass in
+        assert_eq!(
+            Consumer1::<TsDriver>::decl(),
+            Consumer1::<OtherDriver>::decl()
+        );
 
-#[test]
-fn concrete_generic_param() {
-    assert_eq!(
-        MyStruct::<TsDriver>::decl(),
-        "type MyStruct = { u: string, };"
-    );
-    assert_eq!(
-        OtherStruct::<OtherDriver>::decl_concrete(),
-        "type OtherStruct = { u: Foo, x: OtherDriver, };"
-    );
+        assert_eq!(
+            Consumer2::<OtherDriver>::decl_concrete(),
+            "type Consumer2 = { info: OtherInfo, driver: OtherDriver, };"
+        );
+    }
 }
