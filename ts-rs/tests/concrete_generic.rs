52c779ad3e2bdde2e7f80835d87518a7a0183f11
Moritz Bischof <moritz.bischof1@gmail.com>
2024-03-12 19:02:43 +0100

    add simple tests without associated types

--- ts-rs/tests/concrete_generic.rs
+++ ts-rs/tests/concrete_generic.rs
@@ -13,26 +13,26 @@ mod issue_261 {
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/")]
+    #[ts(export, export_to = "concrete_generic/issue_261")]
     struct OtherInfo {
         x: i32,
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/")]
+    #[ts(export, export_to = "concrete_generic/issue_261")]
     struct OtherDriver;
     impl Driver for OtherDriver {
         type Info = OtherInfo;
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/", concrete(T = TsDriver))]
+    #[ts(export, export_to = "concrete_generic/issue_261", concrete(T = TsDriver))]
     struct Consumer1<T: Driver> {
         info: T::Info,
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/", concrete(T = OtherDriver))]
+    #[ts(export, export_to = "concrete_generic/issue_261", concrete(T = OtherDriver))]
     struct Consumer2<T: Driver> {
         info: T::Info,
         driver: T,
@@ -54,5 +54,29 @@ mod issue_261 {
             Consumer2::<OtherDriver>::decl_concrete(),
             "type Consumer2 = { info: OtherInfo, driver: OtherDriver, };"
         );
+    }
+}
+
+mod simple {
+    use ts_rs::TS;
+
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/simple")]
+    #[ts(concrete(T = i32))]
+    struct Simple<T> {
+        t: T,
+    }
+
+    #[derive(TS)]
+    #[ts(export, export_to = "concrete_generic/simple")]
+    #[ts(concrete(T = i32))]
+    struct WithOption<T> {
+        opt: Option<T>,
+    }
+    
+    #[test]
+    fn simple() {
+        assert_eq!(Simple::<String>::decl(), "type Simple = { t: number, }");
+        assert_eq!(WithOption::<String>::decl(), "type Simple = { opt: number | null, }");
     }
 }
