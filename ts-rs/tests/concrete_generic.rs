bf0327cf605a40c06e0b4ad97e1c82e9684a4dfc
Gustavo <gustavo.shigueo@proton.me>
2024-03-12 15:45:06 -0300

    Prevent generation of file in issue261 test

--- ts-rs/tests/concrete_generic.rs
+++ ts-rs/tests/concrete_generic.rs
@@ -13,26 +13,26 @@ mod issue_261 {
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/issue_261")]
+    #[ts(export, export_to = "concrete_generic/issue_261/")]
     struct OtherInfo {
         x: i32,
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/issue_261")]
+    #[ts(export, export_to = "concrete_generic/issue_261/")]
     struct OtherDriver;
     impl Driver for OtherDriver {
         type Info = OtherInfo;
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/issue_261", concrete(T = TsDriver))]
+    #[ts(export, export_to = "concrete_generic/issue_261/", concrete(T = TsDriver))]
     struct Consumer1<T: Driver> {
         info: T::Info,
     }
 
     #[derive(TS)]
-    #[ts(export, export_to = "concrete_generic/issue_261", concrete(T = OtherDriver))]
+    #[ts(export, export_to = "concrete_generic/issue_261/", concrete(T = OtherDriver))]
     struct Consumer2<T: Driver> {
         info: T::Info,
         driver: T,
