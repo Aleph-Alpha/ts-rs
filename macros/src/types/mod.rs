f37e51abbf3b4afc6dc2fdea72100ed0ee463a06
Aiden McClelland <me@drbonez.dev>
2024-03-27 11:46:17 -0600

    use early return

--- macros/src/types/mod.rs
+++ macros/src/types/mod.rs
@@ -20,19 +20,19 @@ pub(crate) fn struct_def(s: &ItemStruct) -> Result<DerivedTS> {
 fn type_def(attr: &StructAttr, ident: &Ident, fields: &Fields) -> Result<DerivedTS> {
     let name = attr.rename.clone().unwrap_or_else(|| to_ts_ident(ident));
     if let Some(t_o) = &attr.type_override {
-        type_override::type_override_struct(attr, &name, t_o)
-    } else {
-        match fields {
-            Fields::Named(named) => match named.named.len() {
-                0 => unit::empty_object(attr, &name),
-                _ => named::named(attr, &name, named),
-            },
-            Fields::Unnamed(unnamed) => match unnamed.unnamed.len() {
-                0 => unit::empty_array(attr, &name),
-                1 => newtype::newtype(attr, &name, unnamed),
-                _ => tuple::tuple(attr, &name, unnamed),
-            },
-            Fields::Unit => unit::null(attr, &name),
-        }
+        return type_override::type_override_struct(attr, &name, t_o);
+    }
+
+    match fields {
+        Fields::Named(named) => match named.named.len() {
+            0 => unit::empty_object(attr, &name),
+            _ => named::named(attr, &name, named),
+        },
+        Fields::Unnamed(unnamed) => match unnamed.unnamed.len() {
+            0 => unit::empty_array(attr, &name),
+            1 => newtype::newtype(attr, &name, unnamed),
+            _ => tuple::tuple(attr, &name, unnamed),
+        },
+        Fields::Unit => unit::null(attr, &name),
     }
 }
