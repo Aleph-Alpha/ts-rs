b247bfa172d6492247cf302c1842948a205c3868
Moritz <moritz.bischof1@gmail.com>
2024-04-02 23:34:39 +0200

    re-implement dependencies to avoid duplicates

--- macros/src/deps.rs
+++ macros/src/deps.rs
@@ -1,54 +1,89 @@
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
+use std::collections::HashSet;
+use std::rc::Rc;
 use syn::{Path, Type};
 
 pub struct Dependencies {
-    dependencies: Vec<TokenStream>,
     crate_rename: Path,
-    pub types: Vec<Type>,
+
+    // Types which are used in the Rust definition. This is tracked to generate a good `where`
+    // bound later
+    used_types: HashSet<Rc<Type>>,
+    // Types on which the type has a dependency, making it show up in the resulting `TypeList`.
+    direct_deps: HashSet<Rc<Type>>,
+    // Types whose dependencies we depend on. The `dependency_types()` of every type in this set
+    // is appended to the resulting `TypeList`
+    transitive_deps: HashSet<Rc<Type>>,
+    // Types whose generics we depend on. The `generics()` of every type in this set is appended
+    // to the resulting `TypeList`
+    generics_deps: HashSet<Rc<Type>>,
 }
 
 impl Dependencies {
     pub fn new(crate_rename: Path) -> Self {
         Self {
-            dependencies: Vec::default(),
             crate_rename,
-            types: Vec::default(),
+            used_types: Default::default(),
+            direct_deps: Default::default(),
+            transitive_deps: Default::default(),
+            generics_deps: Default::default(),
         }
     }
+
+    pub fn used_types(&self) -> impl Iterator<Item = &Type> {
+        self.used_types.iter().map(Rc::as_ref)
+    }
+
     /// Adds all dependencies from the given type
     pub fn append_from(&mut self, ty: &Type) {
-        let crate_rename = &self.crate_rename;
-        self.dependencies
-            .push(quote![.extend(<#ty as #crate_rename::TS>::dependency_types())]);
-        self.types.push(ty.clone());
+        let ty = Rc::new(ty.clone());
+        self.used_types.insert(ty.clone());
+
+        self.transitive_deps.insert(ty);
     }
 
     /// Adds the given type.
     pub fn push(&mut self, ty: &Type) {
-        let crate_rename = &self.crate_rename;
-        self.dependencies.push(quote![.push::<#ty>()]);
-        self.dependencies.push(quote![
-            .extend(<#ty as #crate_rename::TS>::generics())
-        ]);
-        self.types.push(ty.clone());
+        let ty = Rc::new(ty.clone());
+        self.used_types.insert(ty.clone());
+
+        self.direct_deps.insert(ty.clone());
+        self.generics_deps.insert(ty.clone());
     }
 
-    pub fn append(&mut self, mut other: Dependencies) {
-        if !other.dependencies.is_empty() {
-            self.dependencies.push(quote![.extend(#other)]);
-        }
-        self.types.append(&mut other.types);
+    pub fn append(&mut self, other: Dependencies) {
+        self.used_types.extend(other.used_types.into_iter());
+        self.direct_deps.extend(other.direct_deps.into_iter());
+        self.transitive_deps
+            .extend(other.transitive_deps.into_iter());
+        self.generics_deps.extend(other.generics_deps.into_iter());
     }
 }
 
 impl ToTokens for Dependencies {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let crate_rename = &self.crate_rename;
-        let lines = &self.dependencies;
+        let rename = &self.crate_rename;
+
+        let direct = self.direct_deps.iter().map(|ty| {
+            quote! {
+                .push::<#ty>()
+            }
+        });
+        let transitive = self.transitive_deps.iter().map(|ty| {
+            quote! {
+                .extend(<#ty as #rename::TS>::dependency_types())
+            }
+        });
+        let generics = self.generics_deps.iter().map(|ty| {
+            quote! {
+                .extend(<#ty as #rename::TS>::generics())
+            }
+        });
+
         tokens.extend(quote![{
-            use #crate_rename::typelist::TypeList;
-            ()#(#lines)*
+            use #rename::typelist::TypeList;
+            () #(#direct)* #(#transitive)* #(#generics)*
         }])
     }
 }
