e05e7e182734cd67cc9c6cbeeab2defcde36a00d
Gustavo <gustavo.shigueo@proton.me>
2024-04-03 14:22:14 -0300

    Fix compiler error and add crate_rename

--- macros/src/attr/fn.rs
+++ macros/src/attr/fn.rs
@@ -1,13 +1,20 @@
-use syn::{Ident, Result};
+use syn::{Ident, Result, Path, parse_quote};
 
-use super::{parse_assign_inflection, parse_assign_str, Inflection};
+use super::{parse_assign_inflection, parse_assign_str, Inflection, parse_assign_from_str};
 
 #[derive(Default)]
 pub struct FnAttr {
+    crate_rename: Option<Path>,
     pub args: Args,
     pub export_to: Option<String>,
     pub rename: Option<String>,
     pub rename_all: Option<Inflection>,
+}
+
+impl FnAttr {
+    pub fn crate_rename(&self) -> Path {
+        self.crate_rename.clone().unwrap_or_else(|| parse_quote!(::ts_rs))
+    }
 }
 
 #[derive(Default)]
@@ -31,6 +38,7 @@ impl TryFrom<String> for Args {
 
 impl_parse! {
     FnAttr(input, output) {
+        "crate" => output.crate_rename = Some(parse_assign_from_str(input)?),
         "args" => output.args = parse_assign_str(input)?.try_into()?,
         "export_to" => output.export_to = Some(parse_assign_str(input)?),
         "rename" => output.rename = Some(parse_assign_str(input)?),
