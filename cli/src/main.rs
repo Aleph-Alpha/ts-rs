1518c18f9bf6f1a7c10b074299faba81f5339e30
Gustavo <gustavo.shigueo@proton.me>
2024-04-10 11:20:50 -0300

    Remove *-impl features from CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,14 +8,6 @@ struct Args {
     #[arg(long, short, default_value = "./bindings")]
     output_directory: PathBuf,
 
-    /// Enables all of ts-rs's feature flags
-    #[arg(long)]
-    all_features: bool,
-
-    /// Disables serde compatibility
-    #[arg(long)]
-    disable_serde: bool,
-
     /// Disables warnings caused by using serde attributes that ts-rs cannot process
     #[arg(long)]
     no_warnings: bool,
@@ -27,56 +19,12 @@ struct Args {
     /// Formats the generated TypeScript files
     #[arg(long)]
     format: bool,
-
-    /// Enables chrono compatibility
-    #[arg(long)]
-    chrono: bool,
-
-    /// Enables bigdecimal compatibility
-    #[arg(long)]
-    bigdecimal: bool,
-
-    /// Enables uuid compatibility
-    #[arg(long)]
-    uuid: bool,
-
-    /// Enables bson-uuid compatibility
-    #[arg(long)]
-    bson_uuid: bool,
-
-    /// Enables bytes compatibility
-    #[arg(long)]
-    bytes: bool,
-
-    /// Enables url compatibility
-    #[arg(long)]
-    url: bool,
-
-    /// Enables indexmap compatibility
-    #[arg(long)]
-    indexmap: bool,
-
-    /// Enables ordered-float compatibility
-    #[arg(long)]
-    ordered_float: bool,
-
-    /// Enables heapless compatibility
-    #[arg(long)]
-    heapless: bool,
-
-    /// Enables semver compatibility
-    #[arg(long)]
-    semver: bool,
-
-    /// Enables serde_json compatibility
-    #[arg(long)]
-    serde_json: bool,
 }
 
 macro_rules! feature {
     ($cargo_invocation: expr, $args: expr, { $($field: ident => $feature: literal),* $(,)? }) => {
         $(
-            if $args.$field || $args.all_features {
+            if $args.$field {
                 $cargo_invocation
                     .arg("--features")
                     .arg(format!("ts-rs/{}", $feature));
@@ -88,10 +36,6 @@ macro_rules! feature {
 fn main() {
     let args = Args::parse();
 
-    if args.disable_serde && args.all_features {
-        panic!(r#""--disable-serde" and "--all-features" are not compatible"#)
-    }
-
     let mut cargo_invocation = std::process::Command::new("cargo");
 
     cargo_invocation
@@ -99,25 +43,10 @@ fn main() {
         .arg("export_bindings_")
         .env("TS_RS_EXPORT_DIR", args.output_directory);
 
-    if args.disable_serde {
-        cargo_invocation.arg("--no-default-features");
-    }
-
     feature!(cargo_invocation, args, {
         no_warnings => "no-serde-warnings",
         esm_imports => "import-esm",
         format => "format",
-        chrono => "chrono-impl",
-        bigdecimal => "bigdecimal-impl",
-        uuid => "uuid-impl",
-        bson_uuid => "bson-uuid-impl",
-        bytes => "bytes-impl",
-        url => "url-impl",
-        indexmap => "indexmap-impl",
-        ordered_float => "ordered-float-impl",
-        heapless => "heapless-impl",
-        semver => "semver-impl",
-        serde_json => "serde-json-impl"
     });
 
     cargo_invocation.spawn().unwrap().wait().unwrap();
