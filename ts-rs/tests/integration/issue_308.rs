bc444c7b36e8669b733eabdc88c302db64f403c8
Moritz Bischof <moritz.bischof1@gmail.com>
2024-04-16 16:50:24 +0200

    adjust test

--- ts-rs/tests/integration/issue_308.rs
+++ ts-rs/tests/integration/issue_308.rs
@@ -1,11 +1,15 @@
 use ts_rs::TS;
 
-#[derive(TS)]
-#[ts(export, export_to = "issue_308/")]
-struct MyStruct<A, B>(A, B);
+trait Malicious {
+    fn name() -> String;
+}
 
-impl<A, B> MyStruct<A, B> {
+impl<T> Malicious for T {
     fn name() -> String {
         unimplemented!()
     }
 }
+
+#[derive(TS)]
+#[ts(export, export_to = "issue_308/")]
+struct MyStruct<A, B>(A, B);
