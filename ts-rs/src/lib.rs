95ea95c9c3c02dc9ebd6647915cf9cfcd0cb6134
gustavo-shigueo <gustavo.gsmn@gmail.com>
2024-07-05 14:37:36 -0300

    Change HashMap to export mapped types

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -896,11 +896,11 @@ impl<K: TS, V: TS, H> TS for HashMap<K, V, H> {
     }
 
     fn name() -> String {
-        format!("{{ [key: {}]: {} }}", K::name(), V::name())
+        format!("{{ [key in {}]?: {} }}", K::name(), V::name())
     }
 
     fn inline() -> String {
-        format!("{{ [key: {}]: {} }}", K::inline(), V::inline())
+        format!("{{ [key in {}]?: {} }}", K::inline(), V::inline())
     }
 
     fn visit_dependencies(v: &mut impl TypeVisitor)
