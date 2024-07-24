4344480003f4245225167c20b6eb7f08673f6cb5
gustavo-shigueo <gustavo.gsmn@gmail.com>
2024-07-24 13:08:15 -0300

    Make test more robust by checking if all exports are in file

--- ts-rs/tests/integration/same_file_export.rs
+++ ts-rs/tests/integration/same_file_export.rs
@@ -13,21 +13,34 @@ struct DepB {
 }
 
 #[derive(TS)]
-#[ts(export, export_to = "same_file_export/types.ts")]
+#[ts(export_to = "same_file_export/types.ts")]
 struct A {
     foo: DepA,
 }
 
 #[derive(TS)]
-#[ts(export, export_to = "same_file_export/types.ts")]
+#[ts(export_to = "same_file_export/types.ts")]
 struct B {
     foo: DepB,
 }
 
 #[derive(TS)]
-#[ts(export, export_to = "same_file_export/types.ts")]
+#[ts(export_to = "same_file_export/types.ts")]
 struct C {
     foo: DepA,
     bar: DepB,
     biz: B,
+}
+
+#[test]
+fn test() {
+    A::export_all().unwrap();
+    B::export_all().unwrap();
+    C::export_all().unwrap();
+
+    let contents = std::fs::read_to_string(&A::default_output_path().unwrap()).unwrap();
+
+    assert!(contents.contains(&A::decl()));
+    assert!(contents.contains(&B::decl()));
+    assert!(contents.contains(&C::decl()));
 }
