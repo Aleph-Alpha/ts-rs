6651d920cbe95de0435de9e190930e0b319558c9
Gustavo Shigueo <gustavo.gsmn@gmail.com>
2024-09-07 15:13:21 -0300

    Fix smol_str-impl feature name in lib.rs

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -995,7 +995,7 @@ impl_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
 #[cfg(feature = "bigdecimal-impl")]
 impl_primitives! { bigdecimal::BigDecimal => "string" }
 
-#[cfg(feature = "smolstr-impl")]
+#[cfg(feature = "smol_str-impl")]
 impl_primitives! { smol_str::SmolStr => "string" }
 
 #[cfg(feature = "uuid-impl")]
