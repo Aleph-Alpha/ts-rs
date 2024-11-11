95e04015876d98a8d88549509ddea16403aaef60
Gustavo Shigueo <gustavo.gsmn@gmail.com>
2024-11-10 22:21:31 -0300

    Remove invalid optional from test

--- ts-rs/tests/integration/optional_field.rs
+++ ts-rs/tests/integration/optional_field.rs
@@ -100,8 +100,6 @@ struct OptionalStruct {
     #[ts(optional = nullable)]
     c: Option<i32>,
 
-    // `#[ts(optional)]` on a type that isn't `Option<T>` does nothing
-    #[ts(optional = nullable)]
     d: i32,
 
     e: Foo,
