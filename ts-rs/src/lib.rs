e98966d70c07e149aef309e646204af904f70848
gustavo-shigueo <gustavo.gsmn@gmail.com>
2025-02-21 09:09:47 -0300

    Update README

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -122,7 +122,7 @@
 //! [See CONTRIBUTING.md](https://github.com/Aleph-Alpha/ts-rs/blob/main/CONTRIBUTING.md)
 //!
 //! ## MSRV
-//! The Minimum Supported Rust Version for this crate is 1.63.0
+//! The Minimum Supported Rust Version for this crate is 1.78.0
 
 use std::{
     any::TypeId,
