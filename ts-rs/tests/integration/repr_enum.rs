70e8e93228f75b9f0cbf82926db9639ae2c29c05
gustavo-shigueo <58121396+gustavo-shigueo@users.noreply.github.com>
2025-08-03 18:03:30 -0300

    Add tests for #[ts(repr(enum))]

---
+++ ts-rs/tests/integration/repr_enum.rs
@@ -0,0 +1,37 @@
+use ts_rs::TS;
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", repr(enum))]
+enum Foo {
+    A = 1,
+    B = 2,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", repr(enum))]
+enum Bar {
+    A = 1,
+    B,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", repr(enum))]
+enum Baz {
+    A,
+    B,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/",repr(enum = name))]
+enum Biz {
+    A,
+    B,
+}
+
+#[test]
+fn native_ts_enum_repr() {
+    assert_eq!(Foo::decl(), "enum Foo { A = 1, B = 2 }");
+    assert_eq!(Bar::decl(), "enum Bar { A = 1, B }");
+    assert_eq!(Baz::decl(), "enum Baz { A, B }");
+    assert_eq!(Biz::decl(), "enum Biz { A = \"A\", B = \"B\" }");
+}
