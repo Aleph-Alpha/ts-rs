3cc976c8640c5be86631624977d867507ea29137
gustavo-shigueo <58121396+gustavo-shigueo@users.noreply.github.com>
2025-09-13 12:05:54 -0300

    Test rename_all with repr(enum)

--- ts-rs/tests/integration/repr_enum.rs
+++ ts-rs/tests/integration/repr_enum.rs
@@ -22,10 +22,31 @@ enum Baz {
 }
 
 #[derive(TS)]
-#[ts(export, export_to = "repr_enum/",repr(enum = name))]
+#[ts(export, export_to = "repr_enum/", repr(enum = name))]
 enum Biz {
     A,
     B,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", rename_all = "snake_case", repr(enum = name))]
+enum SnakeCase {
+    EnumVariantFoo,
+    EnumVariantBar,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", rename_all = "camelCase", repr(enum = name))]
+enum CamelCase {
+    EnumVariantFoo,
+    EnumVariantBar,
+}
+
+#[derive(TS)]
+#[ts(export, export_to = "repr_enum/", rename_all = "kebab-case", repr(enum = name))]
+enum KebabCase {
+    EnumVariantFoo,
+    EnumVariantBar,
 }
 
 #[test]
@@ -34,4 +55,7 @@ fn native_ts_enum_repr() {
     assert_eq!(Bar::decl(), "enum Bar { \"A\" = 1, \"B\" }");
     assert_eq!(Baz::decl(), "enum Baz { \"A\", \"B\" }");
     assert_eq!(Biz::decl(), "enum Biz { \"A\" = \"A\", \"B\" = \"B\" }");
+    assert_eq!(SnakeCase::decl(), "enum SnakeCase { \"enum_variant_foo\" = \"enum_variant_foo\", \"enum_variant_bar\" = \"enum_variant_bar\" }");
+    assert_eq!(CamelCase::decl(), "enum CamelCase { \"enumVariantFoo\" = \"enumVariantFoo\", \"enumVariantBar\" = \"enumVariantBar\" }");
+    assert_eq!(KebabCase::decl(), "enum KebabCase { \"enum-variant-foo\" = \"enum-variant-foo\", \"enum-variant-bar\" = \"enum-variant-bar\" }");
 }
